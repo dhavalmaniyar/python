7d3faaf7cd525307a9e1e226f96b4850ad3c8967
dhavalmaniyar <54331181+dhavalmaniyar@users.noreply.github.com>
2020-01-13 12:48:25 +0530

    Create Greatest of 3 no..py

---
+++ Greatest of 3 no..py
@@ -0,0 +1,12 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+ 
+if (a > b) and (a > z):
+   largest = a
+elif (b > a) and (b > a):
+   largest = b
+else:
+   largest = c
+ 
+print("The largest number is",largest)
